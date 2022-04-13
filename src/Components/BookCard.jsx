4d98bbf1f58ad43756df3b48bbc93a38ac96c252
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-04-13 08:56:45 +0530

    added debounce and back button

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const BookCard = ({ book }) => {
     const [bookCover, setBookCover] = useState("");
@@ -12,8 +13,22 @@ export const BookCard = ({ book }) => {
         }
     }, []);
 
+    const handleBookView = () => {
+        for(let key in bookFormat) {
+            if(key === "text/html") {
+                console.log("html");
+                return window.location.href = bookFormat[key];
+            }
+            else if(key === "text/plain") {
+                console.log("text");
+                return window.location.href = bookFormat[key];
+            }
+        }
+        return alert("No viewable version available");
+    }
+
     return (
-        <div className="w-auto">
+        <div className="w-auto cursor-pointer" onClick={handleBookView}>
             <img className="sm:h-16 md:h-56 lg:h-56 w-auto rounded-lg" src={bookCover} />
             <div className="text-[#333333] text-sm text-left pt-2">{book.title}</div>
             <div className="text-[#A0A0A0] text-xs text-left">{book.authors[0].name}</div>
--- src/Components/BookPage.jsx
+++ src/Components/BookPage.jsx
@@ -2,9 +2,49 @@ import { Link, useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import { BookCard } from "./BookCard";
 
+function useDebounce(value, delay) {
+    const [debounceValue, setDebounceValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebounceValue(value);
+        }, delay);
+        return () => {
+            clearTimeout(handler);
+        }  
+    }, [value, delay]);
+    return debounceValue;
+}
+
 export const BookPage = () => {
     let { genre } = useParams();
     const [books, setBooks] = useState([]);
+
+    const [searchInput, setSearchInput] = useState("");
+    const debounce = useDebounce(searchInput, 500);
+
+    useEffect(() => {
+        //console.log(debounce);
+        if(searchInput.length > 0) {
+            if(debounce) {
+                getSearchedBook();
+            }
+        }
+        else if(searchInput.length === 0) {
+            getBookData();
+        }
+    }, [debounce, searchInput]);
+
+    async function getSearchedBook() {
+        try {
+            let res = await fetch(`https://gutendex.com/books/?topic=${genre}&&search=${searchInput.toLowerCase()}`);
+            let bookData = await res.json();
+            setBooks(bookData.results);
+        }
+        catch(err) {
+            setBooks([]);
+        }
+    }
 
     useEffect(() => {
         getBookData();
@@ -25,7 +65,14 @@ export const BookPage = () => {
     return(
         <div className="mt-28 justify-center">
             <div className="w-9/12 m-auto">
-                <h2 className="text-[#5E56E7] text-4xl text-left capitalize">{genre}</h2>
+                <div className="flex">
+                    <Link to="/">
+                        <img src="/back_btn.jpg" alt="back" className="w-12 h-12" />
+                    </Link>   
+                    <h2 className="text-[#5E56E7] text-4xl text-left capitalize">{genre}</h2>
+                </div>
+                <input className="border-2 text-left justify-left w-full mt-6 px-6 py-1 focus:outline-none"
+                 type="text" placeholder="Search" onChange={e => setSearchInput(e.target.value)} />
             </div>  
             <div className="w-9/12 m-auto grid lg:gap-x-6 md:gap-x-4 sm:gap-x-1 gap-y-10 lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 mt-16">
                 {books.map((el => (
